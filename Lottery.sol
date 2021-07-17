d942cafb69bbb4a3fdfa3e82b3c9ad92aa75e485
Ryan Slater <78631176+ryanrslater@users.noreply.github.com>
2021-07-17 16:08:50 +1000

    First Upload
    Initial creation of the Lottery app, hasn't been debugged

--- Lottery.sol
+++ Lottery.sol
@@ -1,1 +1,74 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.6;
 
+import "https://github.com/smartcontractkit/chainlink/blob/develop/contracts/src/v0.8/VRFConsumerBase.sol";
+
+/**
+ * THIS IS AN EXAMPLE CONTRACT WHICH USES HARDCODED VALUES FOR CLARITY.
+ * PLEASE DO NOT USE THIS CODE IN PRODUCTION.
+ */
+contract Lottery is VRFConsumerBase {
+
+    address[] Players;
+    mapping(address => uint256) public playerBalance;
+    uint256 public contractBalance;
+
+
+    modifier bettingAmount {
+        require(msg.value == 1);
+        _;
+    }
+
+    function enter() public payable bettingAmount {
+        Players.push(msg.sender);
+        contractBalance = contractBalance + msg.value;
+    }
+    
+    bytes32 internal keyHash;
+    uint256 internal fee;
+    
+    uint256 public randomResult;
+    
+    /**
+     * Constructor inherits VRFConsumerBase
+     * 
+     * Network: Kovan
+     * Chainlink VRF Coordinator address: 0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9
+     * LINK token address:                0xa36085F69e2889c224210F603D836748e7dC0088
+     * Key Hash: 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4
+     */
+    constructor() 
+        VRFConsumerBase(
+            0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9, // VRF Coordinator
+            0xa36085F69e2889c224210F603D836748e7dC0088  // LINK Token
+        ) public
+    {
+        keyHash = 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4;
+        fee = 0.1 * 10 ** 18; // 0.1 LINK (Varies by network)
+    }
+    
+    /** 
+     * Requests randomness 
+     */
+    function getRandomNumber() public returns (bytes32 requestId) {
+        require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract with faucet");
+        return requestRandomness(keyHash, fee);
+    }
+
+    /**
+     * Callback function used by VRF Coordinator
+     */
+    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+        randomResult = randomness % Players.length;
+    }
+
+    function pickWinner() {
+        playerBalance[Players[randomResult]] = contractBalance;
+        contractBalance = 0;
+    }
+    function withdrawBalance() {
+        payable(msg.sender).transfer(playerBalance[msg.sender]);
+    }
+
+    // function withdrawLink() external {} - Implement a withdraw function to avoid locking your LINK in the contract
+}
